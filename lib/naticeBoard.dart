7136377b2e172c71015c26e575eca82232ea6611
ZebaCharoliyaa <charoliyazeba2003@gmail.com>
2025-03-15 10:38:59 +0530

    commit

--- lib/naticeBoard.dart
+++ lib/naticeBoard.dart
@@ -460,8 +460,6 @@
 //   }
 // }
 
-
-
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:school/services/api_services.dart';
@@ -521,17 +519,26 @@ class _NoticeBoardState extends State<NoticeBoard> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back),
+          color: Colors.white,
+        ),
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
         ),
         centerTitle: true,
         backgroundColor: Colors.deepPurple,
-        title: const Text('Notice Board', style: TextStyle(color: Colors.white)),
+        title:
+            const Text('Notice Board', style: TextStyle(color: Colors.white)),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: isLoading
-            ? const Center(child: CircularProgressIndicator()) // ✅ Show loading indicator
+            ? const Center(
+                child: CircularProgressIndicator()) // ✅ Show loading indicator
             : notices.isEmpty
                 ? const Center(child: Text("No notices available."))
                 : GridView.count(
